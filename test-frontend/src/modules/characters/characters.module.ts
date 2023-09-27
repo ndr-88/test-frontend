8d772065f178f64fcd554596dc86787c5ec3f1e0
Andrea Savo <andrea.savo@maggioli.it>
2023-09-27 22:28:17 +0200

    added charactersSandbox as provider in characters module

--- test-frontend/src/modules/characters/characters.module.ts
+++ test-frontend/src/modules/characters/characters.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CharactersSandbox } from 'src/sandbox/characters.sandbox';
 import { CharactersContainerComponent } from '../../containers/characters-container/characters-container.component';
 import { CardModule } from '../card/card.module';
 
@@ -7,5 +8,6 @@ import { CardModule } from '../card/card.module';
   declarations: [CharactersContainerComponent],
   imports: [CommonModule, CardModule],
   exports: [CharactersContainerComponent],
+  providers: [CharactersSandbox],
 })
 export class CharactersModule {}
