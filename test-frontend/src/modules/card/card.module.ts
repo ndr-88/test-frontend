420b2b861b7d68c23ee60cf86fe215b51740779e
Andrea Savo <andrea.savo@maggioli.it>
2023-09-27 08:52:19 +0200

    card Module creation

--- test-frontend/src/modules/card/card.module.ts
+++ test-frontend/src/modules/card/card.module.ts
@@ -1,15 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 
-
-
 @NgModule({
-  declarations: [
-    CardComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CardComponent],
+  imports: [CommonModule],
+  exports: [CardComponent],
 })
-export class CardModule { }
+export class CardModule {}
