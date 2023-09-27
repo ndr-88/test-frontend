d82efd973a0ff086d2fb4032ac7bcef891fd3356
Andrea Savo <andrea.savo@maggioli.it>
2023-09-27 22:26:02 +0200

    colors added in tailwind config

--- test-frontend/tailwind.config.js
+++ test-frontend/tailwind.config.js
@@ -2,10 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    screens: {
+      xl: "1440px",
+      "2xl": "1919px",
+    },
     extend: {
       fontFamily: {
         "playfair-display": ["'Playfair Display'", "serif"],
         "open-sans": ["'Open Sans", "sans-serif"],
+      },
+      colors: {
+        "heading-color-700": "#3C6F75",
+        "subtitle-color-700": "#69A8AF",
+        "description-color": "#275054",
       },
     },
   },
