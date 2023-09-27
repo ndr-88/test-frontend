bc63be766b5de0d5fab09b8d4dc2368cf55c1561
Andrea Savo <andrea.savo@maggioli.it>
2023-09-27 22:27:53 +0200

    character sandbox created

--- test-frontend/src/sandbox/characters.sandbox.ts
+++ test-frontend/src/sandbox/characters.sandbox.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CharactersActions } from 'src/app/actions/characters.actions';
+import { CharactersState } from 'src/app/reducers/characters.reducer';
+import * as CharactersSelectors from 'src/app/selectors/characters.selectors';
 import { CharactersService } from 'src/services/characters.service';
 
 @Injectable()
 export class CharactersSandbox {
-  constructor(private charactersService: CharactersService) {}
+  CharactersSelector$ = this.store.select(
+    CharactersSelectors.selectCharactersGetDTO
+  );
+
+  constructor(
+    private charactersService: CharactersService,
+    private store: Store<CharactersState>
+  ) {}
 
   getCharacters() {
-    this.charactersService.getCharacters().subscribe((res) => {
-      console.log(res);
+    this.charactersService.getCharacters().subscribe((charactersGetDTO) => {
+      this.store.dispatch(
+        CharactersActions.loadCharactersSuccess({ charactersGetDTO })
+      );
     });
   }
 }
