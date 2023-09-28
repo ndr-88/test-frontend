c870bcdd3dc2a9ec10b0435eb7a0ff015d841b0d
Andrea Savo <andrea.savo@maggioli.it>
2023-09-28 12:21:46 +0200

    searchTerm pipe created and added in appModule

--- test-frontend/src/app/app.module.ts
+++ test-frontend/src/app/app.module.ts
@@ -3,18 +3,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
+import { SharedModule } from 'src/components/shared/shared.module';
 import { CharactersModule } from 'src/modules/characters/characters.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { metaReducers, reducers } from './reducers';
+import { SearchTermPipe } from './search-term.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SearchTermPipe],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     CharactersModule,
+    SharedModule,
     StoreModule.forRoot({}, {}),
     StoreRouterConnectingModule.forRoot(),
     StoreModule.forRoot(reducers, { metaReducers }),
