863f9bfd4e74cb976e422171961e9ff8df8bf983
Andrea Savo <andrea.savo@maggioli.it>
2023-09-28 22:35:31 +0200

    subject created for filters

--- test-frontend/src/services/search.service.ts
+++ test-frontend/src/services/search.service.ts
@@ -7,5 +7,10 @@ import { Observable, Subject } from 'rxjs';
 export class SearchService {
   emitSearchValue: Subject<string> = new Subject<string>();
   emitSearchValueObs: Observable<string> = this.emitSearchValue.asObservable();
+  emitFilterValues: Subject<Map<string, string>> = new Subject<
+    Map<string, string>
+  >();
+  emitFilterValuesObs = this.emitFilterValues.asObservable();
+
   constructor() {}
 }
