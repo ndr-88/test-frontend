ce1f5ee4e5b29129b895eb5c258a917c563a7e8a
Andrea Savo <andrea.savo@maggioli.it>
2023-09-28 12:22:27 +0200

    searchTerm moved to sharedModule

--- test-frontend/src/components/shared/shared.module.ts
+++ test-frontend/src/components/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SearchTermPipe } from 'src/app/search-term.pipe';
 import { HeaderComponent } from './header/header.component';
 import { PillComponent } from './pills/pills.component';
 
 @NgModule({
-  declarations: [PillComponent, HeaderComponent],
+  declarations: [PillComponent, HeaderComponent, SearchTermPipe],
   imports: [CommonModule],
-  exports: [PillComponent, HeaderComponent],
+  exports: [PillComponent, HeaderComponent, SearchTermPipe],
 })
 export class SharedModule {}
