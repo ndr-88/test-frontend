d5de498d3a7aff0798d36ea101a7431516f34f3b
Andrea Savo <andrea.savo@maggioli.it>
2023-09-28 23:05:33 +0200

    import InfiniteScrollModule

--- test-frontend/src/modules/characters.module.ts
+++ test-frontend/src/modules/characters.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { CharactersSandbox } from 'src/sandbox/characters.sandbox';
 import { CharactersContainerComponent } from '../containers/characters-container/characters-container.component';
 import { CardModule } from './card.module';
 
 @NgModule({
   declarations: [CharactersContainerComponent],
-  imports: [CommonModule, CardModule],
+  imports: [CommonModule, CardModule, InfiniteScrollModule],
   exports: [CharactersContainerComponent],
   providers: [CharactersSandbox],
 })
