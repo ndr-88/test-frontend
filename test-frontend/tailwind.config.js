24587eb4333375036dbd5d0a27276fadd8e81b47
Andrea Savo <andrea.savo@maggioli.it>
2023-09-28 22:33:45 +0200

    new colors added in tailwind config

--- test-frontend/tailwind.config.js
+++ test-frontend/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    borderColor: {
+      filter: "#3C6F75",
+    },
     screens: {
       xl: "1440px",
       "2xl": "1919px",
@@ -15,6 +18,9 @@ module.exports = {
         "heading-color-700": "#3C6F75",
         "subtitle-color-700": "#69A8AF",
         "description-color": "#275054",
+        filter: "#3C6F75",
+        "filter-screen": "#E0F8FC",
+        "filter-page": "#69A8AF",
       },
     },
   },
