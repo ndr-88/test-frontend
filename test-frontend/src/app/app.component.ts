626b550483551a244276201437db24b08341cbca
Andrea Savo <andrea.savo@maggioli.it>
2023-09-28 12:27:18 +0200

    emitSearchValueObs subscriptiona added in app component

--- test-frontend/src/app/app.component.ts
+++ test-frontend/src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { CharactersSandbox } from 'src/sandbox/characters.sandbox';
+import { SearchService } from 'src/services/search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'Rick and Morty';
-  charactersGetDTO$ = this.charactersSandbox.CharactersSelector$;
+  charactersGetDTO$ = this.charactersSandbox.charactersSelector$;
+  info$ = this.charactersSandbox.info$;
+  searchValue: string;
+  destroy$: Subject<void> = new Subject<void>();
 
-  constructor(private charactersSandbox: CharactersSandbox) {}
+  constructor(
+    private charactersSandbox: CharactersSandbox,
+    private searchService: SearchService
+  ) {
+    this.searchService.emitSearchValueObs
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((text) => {
+        this.searchValue = text;
+      });
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
